55e75dc21f3b60e6cd69d0de45051ecea08340bf
Liam <droog@ualberta.ca>
2021-05-20 12:41:29 -0600

    Implemented methods for hdf5, changed up gui functionality, found more bugs ://

--- droogCNC.py
+++ droogCNC.py
@@ -19,8 +19,8 @@ class TwoAxisStage:
         self.pos = [0., 0.]
         self.currentpos = 'X0 Y0'
         self.rate = 1
-        self.rowLen = 6
-        self.colLen = 5
+        self.rowLen = 5
+        self.colLen = 9
         self.port = port
         self.baud = baud
         self.startupfile = startupfile
@@ -29,29 +29,28 @@ class TwoAxisStage:
         self.tempFile = None
         self.temprunning = False
         self.filename = None
-        self.buttonx = 6
-        self.buttony = 3
+        self.buttonx = 12
+        self.buttony = 6
         self.feedrate = 0
-        self.shotnum = 0    # TODO record me in the temp file and recall
-        self.dro_text = 'X: %1.3f, Y:%1.3f, Feedrate: %d' % (self.pos[0], self.pos[1], self.feedrate)
+        self.shotnum = 0
+        #self.dro_text = 'X: %1.3f, Y:%1.3f, Feedrate: %d' % (self.pos[0], self.pos[1], self.feedrate)
+        self.datafile = None
+        self.datafilename = None
         self.parameters = {
-            'stepPulseLength'   : [0, None],
-            'stepIdleDelay'     : [1, None],
-            'axisDirection'     : [3, None],
-            'statusReport'      : [10, None],
-            'feedbackUnits'     : [13, None],
-            'xSteps/mm'         : [100, None],
-            'ySteps/mm'         : [101, None],
-            'xMaxRate'          : [110, None],
-            'yMaxRate'          : [111, None],
-            'xMaxAcc'           : [120, None],
-            'yMaxAcc'           : [121, None],
-            }
+            'stepPulseLength': [0, None],
+            'stepIdleDelay': [1, None],
+            'axisDirection': [3, None],
+            'statusReport': [10, None],
+            'feedbackUnits': [13, None],
+            'xSteps/mm': [100, None],
+            'ySteps/mm': [101, None],
+            'xMaxRate': [110, None],
+            'yMaxRate': [111, None],
+            'xMaxAcc': [120, None],
+            'yMaxAcc': [121, None],
+        }
 
-        self.param_number = 2
-
-        #self.datafilename = str('-'.join(list(i.replace(':', '-') for i in time.asctime().split(' '))))+'.hdf5'
-        #self.datafile = HDF5File(self.datafilename, 'shot' + str(self.shotnum), self.param_number)
+        self.param_number = 2  ###########
 
         # draws all on-screen controls and assigns their event commands
         self.rowarr = list(i for i in range(self.rowLen))
@@ -108,39 +107,43 @@ class TwoAxisStage:
         self.lbl_frame = tk.Frame(master=self.window, relief=tk.RAISED, borderwidth=3)
         self.lbl_pos = tk.Label(master=self.lbl_frame, text='X: %1.3f, Y:%1.3f, Feedrate: %d'
                                                             % (self.pos[0], self.pos[1], self.feedrate))
-        self.lbl_pos['font'] = font.Font(size=12)
+        self.lbl_pos['font'] = font.Font(size=18)
 
-        self.lbl_frame.grid(row=0, column=0, columnspan=self.colLen+1, sticky='ew')
+        self.lbl_frame.grid(row=0, column=0, columnspan=self.colLen + 1, sticky='ew')
         self.lbl_pos.pack()
 
         # gcode input box
         self.gcode_entry = tk.Entry(master=self.window)
-        self.gcode_entry.grid(row=4, columnspan=3, sticky='ew')
+        self.gcode_entry.configure(width=40)
+        self.gcode_entry.grid(row=4, column=6, sticky='wn')
+
 
         # gcode button
         self.gcode_send = tk.Button(master=self.window, text='Send',
-                                    command=lambda: self.sendCommand(self.gcode_entry.get().rstrip() + '\n', resetarg=True,
+                                    command=lambda: self.sendCommand(self.gcode_entry.get().rstrip() + '\n',
+                                                                     resetarg=True,
                                                                      entry=self.gcode_entry))
-        self.gcode_send.grid(row=4, column=3, sticky='')
-        self.gcode_send.configure(width=self.buttonx, height=self.buttony)
+        self.gcode_send.grid(row=4, column=9, sticky='ewn')
+        self.gcode_send.configure(width=5)
 
         # file input box
         self.file_entry = tk.Entry(master=self.window)
-        self.file_entry.grid(row=5, columnspan=3, sticky='ew')
+        self.file_entry.grid(row=4, columnspan=3, sticky='new')
 
         # file input button
-        self.file_input = tk.Button(master=self.window, text='Get File', command=lambda: self.getFile(self.file_entry.get()))
-        self.file_input.grid(row=5, column=3)
-        self.file_input.configure(width=self.buttonx, height=self.buttony)
+        self.file_input = tk.Button(master=self.window, text='Get File',
+                                    command=lambda: self.getFile(self.file_entry.get()))
+        self.file_input.grid(row=4, column=3, sticky='new')
+        #self.file_input.configure(width=self.buttonx, height=self.buttony)
 
         # file run button
         self.file_run = tk.Button(master=self.window, text='Run', command=lambda: self.runFile())
-        self.file_run.grid(row=5, column=4)
-        self.file_run.configure(width=self.buttonx, height=self.buttony)
-        self.kill_btn = tk.Button(master=self.window, text='Kill', command=self.killSwitch)
+        self.file_run.grid(row=4, column=4, sticky='new')
+        self.file_run.configure(width=self.buttonx)
 
-        self.kill_btn.grid(row=5, column=5)
-        self.kill_btn.configure(width=self.buttonx, height=self.buttony)
+        self.kill_btn = tk.Button(master=self.window, text='Kill', command=self.killSwitch)
+        self.kill_btn.grid(row=4, column=5, sticky='new')
+        self.kill_btn.configure(width=self.buttonx)
 
         self.increment_btn = tk.Button(master=self.window, text='G91', command=self.setG91)
         self.increment_btn.grid(row=1, column=1, sticky='nsew')
@@ -152,7 +155,8 @@ class TwoAxisStage:
 
         # go home button
         self.home_btn = tk.Button(master=self.window, text='Go\nHome', command=lambda: self.sendCommand('G90 X0 Y0 F'
-                                  + str(self.parameters['xMaxRate'][1])))
+                                                                                                        + str(
+            self.parameters['xMaxRate'][1])))
         self.home_btn.grid(row=2, column=4, sticky='nsew')
         self.home_btn.configure(width=self.buttonx, height=self.buttony)
 
@@ -161,12 +165,22 @@ class TwoAxisStage:
         self.set_home.grid(row=1, column=4, sticky='nsew')
         self.set_home.configure(width=self.buttonx, height=self.buttony)
 
-        self.start_from_death_btn = tk.Button(master=self.window, text='No temp\nfile found', command=self.__startFromDeath)
+        self.start_from_death_btn = tk.Button(master=self.window, text='No temp\nfile found',
+                                              command=self.__startFromDeath)
         self.start_from_death_btn.grid(row=2, column=5, sticky='nesw')
-        self.start_from_death_btn['font'] = font.Font(size=7)
+        self.start_from_death_btn['font'] = font.Font(size=10)
         self.start_from_death_btn.configure(width=self.buttonx, height=self.buttony)
 
+        self.output = tk.Label(master=window, height=19, width=3*self.buttonx, anchor=tk.SW, justify=tk.LEFT)
+        self.output.grid(columnspan=4, rowspan=4, row=1, column=6, sticky='new')
+        self.output.configure(bg='white')
+        self.window.bind('<Return>', (lambda x: self.sendCommand(self.gcode_entry.get(), entry=self.gcode_entry,
+                                                                 resetarg=True)))
+
+
+
         self.__setTempFile()
+        self.setKeybinds()
         self.Refresh()
 
     def setKeybinds(self):
@@ -182,7 +196,7 @@ class TwoAxisStage:
         :return: None
         """
         self.lbl_pos.configure(text='X: %1.3f, Y:%1.3f, Feedrate: %d' % (self.pos[0], self.pos[1], self.feedrate))
-        self.window.after(50, self.Refresh)
+        self.window.after(5, self.Refresh)
 
     def onKeyPress(self, event, wasd=False):
         """
@@ -207,9 +221,9 @@ class TwoAxisStage:
         :param v: float rate
         :return: None
         """
-        # increments the X counter by specified v, set by the rate
-        c = 'G91 x' + str(v) + '\n'
-        self.sendCommand(c)
+        if not self.temprunning:
+            c = 'G91 x' + str(v) + '\n'
+            self.sendCommand(c)
 
     def jogY(self, v):
         """
@@ -217,9 +231,9 @@ class TwoAxisStage:
         :param v: float rate
         :return: None
         """
-        # increments the Y counter by specified v, set by the rate
-        c = 'G91 y' + str(v) + '\n'
-        self.sendCommand(c)
+        if not self.temprunning:
+            c = 'G91 y' + str(v) + '\n'
+            self.sendCommand(c)
 
     def switchRate(self, v):
         """
@@ -235,10 +249,10 @@ class TwoAxisStage:
         Reads out the reply from GRBL
         :return: None
         """
-        # todo: Fix borked DRO feedback
         # implement own method?
-        out = self.s.readline()        # Wait for grbl response with carriage return
-        print('> ' + out.strip().decode('UTF-8'))
+        out = self.s.readline()  # Wait for grbl response with carriage return
+        #print('> ' + out.strip().decode('UTF-8'))
+        self.output['text'] += '\n>' + out.decode('UTF-8').strip()
 
     def sendCommand(self, gcode, resetarg=False, entry=None):
         """
@@ -264,11 +278,13 @@ class TwoAxisStage:
             for i in t:
                 if i.lower().strip()[0] == 'f':
                     self.__setFeed(int(i.strip()[1:]))
-            print('Sent: ' + gcode.rstrip())
+
+            #self.output['text'] += '\n' + time.strftime('%H:%M:%S', time.localtime()) + ': ' + gcode.rstrip()
+            self.output['text'] += '\n' + '~> ' + gcode.rstrip()
             gcode = gcode.rstrip() + '\n'
             self.s.write(gcode.encode('UTF-8'))
             self.readOut()
-            #self.__writeData([time.time_ns(), gcode.rstrip()])
+            # self.__writeData([time.time_ns(), gcode.rstrip()])
             self.setPos(gcode)
 
     def initSerial(self, port, baud, filename):
@@ -372,9 +388,10 @@ class TwoAxisStage:
                 if line != '' and line[0] != ';':
                     self.queue.enqueue(line)
             print('Loaded ' + filename)
+            self.output['text'] += '\n~>' + ' Loaded '+filename
         except FileNotFoundError:
             self.file_entry.delete(0, 'end')
-            self.file_entry.insert(0, 'File does not exist')
+            self.output['text'] += '\n!> File does not exist'
 
     def runFile(self):
         """
@@ -416,7 +433,9 @@ class TwoAxisStage:
         """
         if self.s:
             self.queue.clear()
-            print('Current motion was killed')
+            self.temprunning = False
+            self.output['text'] += '\n!> Current motion killed'
+
     def calcDelay(self, currentpos, nextpos):
         """
         Calculates a lower end of the required delay between moved for the queue command system
@@ -424,8 +443,7 @@ class TwoAxisStage:
         :param nextpos: Next position
         :return: time delay in ms
         """
-        #   todo: parse positions from gcode
-        #         parse feeds & speeds from startup file
+        #   todo: parse feeds & speeds from startup file
         #         calculate (approximate?) time delay until next step
         #         circular motion
 
@@ -434,13 +452,13 @@ class TwoAxisStage:
         assert len(ipos) == len(fpos), 'Input arrays must be same length'
 
         v = float(self.parameters['xMaxRate'][1]) / 60
-        a = float(self.parameters['yMaxRate'][1])
+        a = float(self.parameters['xMaxRate'][1]) # becomes very choppy changing to xMaxAcc... idk wtf
 
         delta = list(ipos[i] - fpos[i] for i in range(len(ipos)))
         d = np.sqrt(sum(i ** 2 for i in delta))
         deltaT = ((2 * v) / a) + ((d - (v ** 2 / a)) / v)
         print('next move in ' + str(int(np.floor(deltaT * 1000))) + 'ms')
-        return int(np.floor(deltaT * 1000))    # in ms
+        return int(np.floor(deltaT * 1000))  # in ms
 
     def __parsePosition(self, ipos):
         """
@@ -474,6 +492,8 @@ class TwoAxisStage:
 
         self.feedrate = int(self.parameters['xMaxRate'][1])
         print(self.feedrate)
+        print(self.parameters['xMaxRate'])
+        print(self.parameters['xMaxAcc'])
 
     def __setTempFile(self):
         """
@@ -493,7 +513,7 @@ class TwoAxisStage:
         """
         if self.temprunning:
             if self.queue.size() > 0:
-                np.save(self.tempFile, [self.queue.peek(), time.asctime(), self.filename])
+                np.save(self.tempFile, [self.queue.peek(), time.asctime(), self.filename, self.shotnum])
                 self.window.after(1000, self.__saveTempData)
                 return True
         else:
@@ -504,9 +524,10 @@ class TwoAxisStage:
         Retrieves temp data from temp.npy if it exists and user calls it. Needs to be updated
         to reflect final changes in temp data stored once integrated into laser system
         :return: Last line that runfile stored before unexpected power loss
+        #todo save position and reload it
         """
         self.tempFile = 'temp.npy'
-        currentline, t, self.filename = np.load(self.tempFile)
+        currentline, t, self.filename, self.shotnum = np.load(self.tempFile)
         print(currentline, t, self.filename)
         return currentline
 
@@ -539,7 +560,6 @@ class TwoAxisStage:
             self.start_from_death_btn.configure(text='Not\nconnected')
             self.window.after(5000, lambda: self.start_from_death_btn.configure(text='Load\nData?'))
 
-
     def __removeTempFile(self):
         """
         removes temp file at end of program run
@@ -566,6 +586,36 @@ class TwoAxisStage:
             else:
                 button.configure(fg=c1)
             self.window.after(delay, lambda: self.__blinkButton(button, c1, c2, delay))
+
+    def __createDataFile(self):
+        self.datafilename = str('-'.join(list(i.replace(':', '-') for i in time.asctime().split(' ')))) + '.hdf5'
+        self.datafile = HDF5File(self.datafilename)
+
+    def __createGroup(self, name):
+        self.datafile.createGroup(name)
+
+    def __setMetadataFromFile(self, mdfile, path='/'):
+        metadata = self.__parseMetadataFile(mdfile)
+        for key, value in metadata.items():
+            self.datafile.setMetadata(key, value, path=path)
+
+    def __setMetadata(self, key, value, path='/'):
+        self.datafile.setMetadata(key, value, path=path)
+
+    def __createDataSet(self, Group, Data):
+        self.datafile.create_dataset(Group, Data, Data.shape)
+
+    def __parseMetadataFile(self, filename):
+        md = {}
+        try:
+            with open(filename, 'r') as f:
+                for i in f.readlines():
+                    j = i.rstrip().split(';')
+                    md[j[0]] = j[1]
+            return md
+
+        except:
+            print('metadata file broken, fix and try again')
 
     def __writeData(self, data):
         """
@@ -650,81 +700,16 @@ class Queue:
         """
         str_exp = ""
         for item in self.__items:
-            str_exp += ('> ' +str(item))
+            str_exp += ('> ' + str(item))
         return str_exp
 
 
-# class HDF5File:
-#     """
-#     Simple class to append value to a hdf5 file on disc (usefull for building keras datasets)
-#
-#     Params:
-#         datapath: filepath of h5 file
-#         dataset: dataset name within the file
-#         shape: dataset shape (not counting main/batch axis)
-#         dtype: numpy dtype
-#
-#     Usage:
-#         hdf5_store = HDF5Store('/tmp/hdf5_store.h5','X', shape=(20,20,3))
-#         x = np.random.random(hdf5_store.shape)
-#         hdf5_store.append(x)
-#         hdf5_store.append(x)
-#
-#     From https://gist.github.com/wassname/a0a75f133831eed1113d052c67cf8633
-#     """
-#
-#     def __init__(self, datapath, dataset, paramnum, compression='gzip'):
-#         self.datapath = datapath
-#         self.dataset = dataset
-#         #self.shape = (paramnum,)
-#         self.shape = paramnum
-#         self.i = 0
-#
-#         with h5py.File(self.datapath, mode='a') as h5f:
-#             self.dset = h5f.create_dataset(
-#                 dataset,
-#                 shape=(0,) + self.shape,
-#                 maxshape=(None,) + self.shape,
-#                 compression=compression)
-#
-#     def append(self, values):
-#         # different datatypes (ie float, int, string) playing nice together
-#         with h5py.File(self.datapath, mode='a') as h5f:
-#             dset = h5f[self.dataset]
-#             dset.resize((self.i + 1,) + self.shape)
-#             dset[self.i] = [values]
-#             self.i += 1
-#             h5f.flush()
-#
-#     def setMetadata(self, attribute, value):
-#         with h5py.File(self.datapath, mode='a') as h5f:
-#             h5f.attrs[attribute] = value
-#     def createDataset(self, group, dataset, size, compression):
-
 class HDF5File:
-    """
-    Simple class to append value to a hdf5 file on disc (usefull for building keras datasets)
-
-    Params:
-        datapath: filepath of h5 file
-        dataset: dataset name within the file
-        shape: dataset shape (not counting main/batch axis)
-        dtype: numpy dtype
-
-    Usage:
-        hdf5_store = HDF5Store('/tmp/hdf5_store.h5','X', shape=(20,20,3))
-        x = np.random.random(hdf5_store.shape)
-        hdf5_store.append(x)
-        hdf5_store.append(x)
-
-    Adapted From https://gist.github.com/wassname/a0a75f133831eed1113d052c67cf8633
-    """
 
     def __init__(self, datapath):
         self.datapath = datapath
 
     def append(self, group, dataset, values):
-        # different datatypes (ie float, int, string) playing nice together
         with h5py.File(self.datapath, mode='a') as h5f:
             i = 0
             dset = h5f[group][dataset]
@@ -733,9 +718,9 @@ class HDF5File:
             i += 1
             h5f.flush()
 
-    def setMetadata(self, attribute, value):
+    def setMetadata(self, attribute, value, path='/'):
         with h5py.File(self.datapath, mode='a') as h5f:
-            h5f.attrs[attribute] = value
+            h5f[path].attrs[attribute] = value
 
     def createDataset(self, group, dataset, shape, compression='gzip'):
         with h5py.File(self.datapath, mode='a') as h5f:
