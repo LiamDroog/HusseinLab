abd1bd406ccb1dbab7010d543e9d9aaf9d56ef8e
Liam <droog@ualberta.ca>
2021-05-21 14:24:07 -0600

    Refined existing methods. Added tree - readout functionality

--- HDF5Methods.py
+++ HDF5Methods.py
@@ -20,7 +20,7 @@ def createGroup(filename, groupname):
         h5f.create_group(groupname)
 
 
-def createDataset(filename, group, name, data, compression='gzip'):
+def createDataset(filename, name, data, group='/', compression='gzip'):
     with h5py.File(filename, mode='a') as h5f:
         h5f[group].create_dataset(
             name,
@@ -33,11 +33,33 @@ def getMetadata(filename, path='/'):
         return list(h5f[path].attrs.items())
 
 
-def getData(filename, path, dataset):
+def getData(filename, dataset, path=None):
+    if path is None:
+        path = '/'
     with h5py.File(filename, mode='a') as h5f:
         return h5f[path][dataset][()]
 
 
-def getSubFolders(filename, path='/'):
+def getSubGroups(filename, path='/'):
     with h5py.File(filename, mode='a') as h5f:
         return list(h5f[path].keys())
+
+
+def tree(filename):
+    with h5py.File(filename, mode='a') as h5f:
+        h5f.visititems(__print_attrs)
+
+
+def __print_attrs(name, obj):
+    try:
+        print('/' + name)
+        n = 5
+        print(' ' * n + 'Metadata:')
+
+        for key, val in obj.attrs.items():
+            print(' ' * n + "%s: %s" % (key, val))
+        print(obj[()])
+        print('\n')
+
+    except Exception as e:
+        print(e)
